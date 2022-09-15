305b4b9ac13964d4a5f5dba13139521f5756cda4
miller001155 <miller00115@gmail.com>
2022-09-15 14:26:34 +0300

    homework2-new

--- homework.py
+++ homework.py
@@ -92,3 +92,4 @@ number_greater_than_100 = int(input("введите число больше ст
 number_less_than_10 = int(input("введите число меньше 10:"))  # с помощью input запрашиваем число
 number_of_occurrences = number_greater_than_100 // number_less_than_10  # переменная с целочислунным делением
 print(f'меньшее число помещается в большее {number_of_occurrences} раз!')  # с помощью F строк выводим результат
+#
