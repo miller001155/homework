bc829c7423d4db4c16177f150392fbeedd93a8f8
miller001155 <miller00115@gmail.com>
2022-10-12 13:04:05 +0300

    homework5

--- homework5/class_Car.py
+++ homework5/class_Car.py
@@ -4,6 +4,10 @@ class Car:
         self.model = model
         self.year = year
         self.speed = 0
+    def get_descriptive_name(self):
+        '''Возвращает аккуратно отформотированное описание'''
+        long_name = f'{self.year} {self.stamp} {self.model}'
+        return long_name.title()
 
     def speed_up(self):
         self.speed += 5
@@ -19,6 +23,9 @@ class Car:
 
     def reverse_gear(self):
         self.speed = - self.speed
+
+    def speed_limit(self):
+        self.speed = f'100 км/ч'
 
 
 opel = Car('opel', 'Astra', 2010,)
--- homework5/newCar.py
+++ homework5/newCar.py
@@ -1,2 +1,6 @@
-import class_Car
+import class_Car  # импортируем модуль из предыдущего упражнения
+my_mercedes = class_Car.Car('Mercedes', 'E500', 2000)  # создаем объект из класса Car
+print(my_mercedes.get_descriptive_name())  # вывод информации на экран
+my_mercedes.speed_limit()  # вводим новую функцию в предыдущем классе и используем ее
+my_mercedes.speed_display()  # выводим скорость на экран
 
