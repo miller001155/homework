757843f83be0bdc9c117d0faf22164d71ee39956
miller001155 <miller00115@gmail.com>
2022-10-14 23:03:14 +0300

    turtle

---
+++ homework6/turtle/task_60-68.py
@@ -0,0 +1,30 @@
+import turtle
+'''задача 60'''
+for i in range(0, 4):
+    turtle.forward(100)
+    turtle.right(90)
+turtle.exitonclick()
+
+'''задача 61'''
+
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+
+turtle.left(120)
+turtle.forward(100)
+turtle.exitonclick()
+
+'''задача 62'''
+turtle.circle(100)
+turtle.exitonclick()
+
+'''задача 63'''
+
+
+
+'''задача 64'''
+for i in range(5):
+    turtle.forward(100)
+    turtle.right(144)
+turtle.exitonclick()
