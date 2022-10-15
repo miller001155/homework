6a30a11a8d126f1340a680fdc515d290bc657b99
miller001155 <miller00115@gmail.com>
2022-10-15 14:17:59 +0300

    60-68

--- homework6/turtle/workout.py
+++ homework6/turtle/workout.py
@@ -1,6 +1,10 @@
 import turtle
 
-for i in range(5):
-    turtle.forward(100)
-    turtle.right(144)
+
+
+for i in range(0, 10):
+    turtle.right(36)
+    for i in range(8):
+        turtle.forward(100)
+        turtle.left(45)
 turtle.exitonclick()
