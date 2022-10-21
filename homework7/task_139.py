2252953b1bcda2cf4ed3bf920ae2562407985eaf
miller001155 <miller00115@gmail.com>
2022-10-21 13:18:26 +0300

    task_139_2

--- homework7/task_139.py
+++ homework7/task_139.py
@@ -4,21 +4,29 @@ with sq.connect('PhoneBook.db') as con:
     cur = con.cursor()
 
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS names (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    First name TEXT,
-    Surname TEXT,
-    Phone Number INTEGER
+        CREATE TABLE IF NOT EXISTS names (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        First_name TEXT,
+        Surname TEXT,
+        Phone_Number INTEGER
     )
     """
                 )  # создание таблицы
     con.commit()  # сохранение изменений
 
     cur.execute('''
-    INSERT INTO names (id, First, Surname, Phone) VALUES (1,'Simon', 'Howeis', 01223349752);
-    INSERT INTO names (id, First, Surname, Phone) VALUES (2, 'Karen', 'Philips', 01954295773);
-    INSERT INTO names (id, First, Surname, Phone) VALUES (3,'Darren', 'Smith', 01583749012);
-    INSERT INTO names (id, First, Surname, Phone) VALUES (4, 'Anne', 'Jones', 01323567322);
-    INSERT INTO names (id, First, Surname, Phone) VALUES (5, 'Mark', 'Smith', 01223855534);
-    ''')  # добавление данных в таблицу
+        INSERT INTO names (id, First, Surname, Phone) VALUES (1,'Simon', 'Howeis', 01223349752);
+ ''')  # добавление данных в таблицу
+    cur.execute('''
+        INSERT INTO names (id, First, Surname, Phone) VALUES (2, 'Karen', 'Philips', 01954295773);
+    ''')
+    cur.execute('''
+        INSERT INTO names (id, First, Surname, Phone) VALUES (3,'Darren', 'Smith', 01583749012);
+ ''')
+    cur.execute('''
+        INSERT INTO names (id, First, Surname, Phone) VALUES (4, 'Anne', 'Jones', 01323567322);
+    ''')
+    cur.execute('''
+        INSERT INTO names (id, First, Surname, Phone) VALUES (5, 'Mark', 'Smith', 01223855534);
+    ''')
     con.commit()  # сохранение изменений
